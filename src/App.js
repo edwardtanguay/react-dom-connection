1181c9db933439210fbaf87b821981596d80dee4
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-02-03 22:42:16 +0100

    finished

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import { useRef, useEffect } from 'react';
+import './App.scss';
+
+function App() {
+
+	const pianoElem = useRef(null);
+	const contents = useRef(null);
+
+	// class JavaScript function
+	const showIt = () => {
+		if (contents.current.innerHTML === '') {
+			contents.current.innerHTML = 'succeeded';
+		} else {
+			contents.current.innerHTML = '';
+		}
+	}
+
+	useEffect(() => {
+
+		// make classic DOM element
+		const para = document.createElement("p");
+		para.innerText = "click this text";
+		para.classList.add('clickText');
+
+		// attach DOM element to page 
+		pianoElem.current.appendChild(para);
+
+		// manipulate DOM element
+		pianoElem.current.style.color = 'red';
+
+		// attach even to DOM element
+		pianoElem.current.addEventListener("click", showIt)
+
+	}, []);
+
+	return (
+		<div className="App">
+			<h1>Testing DOM to Vanilla JavaScript</h1>
+			<div ref={pianoElem}></div>
+			<div ref={contents}></div>
+		</div>
+	);
+}
+
+export default App;
